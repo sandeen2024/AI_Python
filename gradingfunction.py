baed91dd20b51d570e28fb31a4a8191386ffda63
sandeen2024 <sandeen@metropolia.fi>
2025-03-23 19:45:48 +0200

    ok

---
+++ gradingfunction.py
@@ -0,0 +1,11 @@
+def medicine_eligibility(age, weight):
+    if age < 18 and weight < 55:
+        return  "Sorry,This medicine cannot be given to you."
+    elif weight >= 55 and age >= 18:
+       return "Here is your medicine. Get well soon!"
+    else:
+       return "Sorry,This medicine cannot be given to you."
+age = int(input("Please enter your age: "))
+weight = int(input("weight(kg): "))
+result = medicine_eligibility(age, weight)
+print(result)
